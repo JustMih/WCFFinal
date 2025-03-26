bf7110724de86574c7c8a1c9af822442a3609a22
Kamasho <kamashogodfrey997@gmail.com>
2025-03-26 11:18:43 +0300

    go live

--- src/pages/call-center-pages/call-center-supervisor-chat/CallCenterSupervisorChat.js
+++ src/pages/call-center-pages/call-center-supervisor-chat/CallCenterSupervisorChat.js
@@ -10,10 +10,10 @@ import {
 import { baseURL } from "../../../config";
 
 // live
-// const socket = io("http://10.57.0.16:5070");
+const socket = io("http://10.57.0.16:5070");
 
 // test
-const socket = io("http://127.0.0.1:5070"); // Connect to backend
+// const socket = io("http://127.0.0.1:5070"); // Connect to backend
 
 const CallCenterSupervisorChat = () => {
   const [messages, setMessages] = useState([]);
