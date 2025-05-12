68a9679dbb44cf95f3e8880a0689389fdc9a017b
Kamasho <kamashogodfrey997@gmail.com>
2025-05-12 14:58:57 +0300

    update url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // on development
-export const baseURL = "http://127.0.0.1:5070/api";
+// export const baseURL = "http://127.0.0.1:5070/api";
 
 
 // on test server
-  // export const baseURL = "http://10.52.0.19:5070/api";
+  export const baseURL = "http://10.52.0.19:5070/api";
