fd6d19f451e9f4abc0787f242493d00da858781f
root <root@WCF-CC>
2025-06-20 13:20:09 +0300

    new live dashbaord

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // on development
- export const baseURL = "http://127.0.0.1:5070/api";
+//  export const baseURL = "http://127.0.0.1:5070/api";
 
 // on test server
-// export const baseURL = "https://10.52.0.19/api";
+export const baseURL = "https://10.52.0.19/api";
 // 
