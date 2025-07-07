1ee3b60374ead3340812974507197d3bf975bde5
reynah762 <reynah762@gmail.com>
2025-07-07 14:09:59 +0300

    view claim in laravel

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:3000", "https://10.52.0.19:3000"],
+  origin: ["https://localhost:3000", "https://10.52.0.19:3000"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "Accept"]
