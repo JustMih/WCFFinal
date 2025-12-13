bdb7c9218ffe83716cd048a16b9ac29fe756e9e0
reynah762 <reynah762@gmail.com>
2025-12-13 06:13:47 +0300

    changes on cobfig file

--- src/config.js
+++ src/config.js
@@ -1,8 +1,4 @@
 // on development
-
-export const baseURL = "http://127.0.0.1:5070/api";
-// export const baseURL = "https://10.52.0.19/api";
-//  export const baseURL = "https://192.168.1.170/api";
-export const amiURL = "http://127.0.0.1:5070/api";
-// export const amiURL = "https://10.52.0.19/api";
+export const baseURL = "https://192.168.21.70/api";
+export const amiURL = "http://192.168.21.70:5075";
  
