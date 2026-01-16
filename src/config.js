252964579664497e8eeebaab4be5f57855f934ac
Kamasho <kamashogodfrey997@gmail.com>
2026-01-16 17:29:10 +0300

    s test

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // on development
-export const baseURL = "http://192.168.21.70:5070/api";
+export const baseURL = "https://192.168.21.70/api";
 export const amiURL = "http://192.168.21.70:5075";
 
 
