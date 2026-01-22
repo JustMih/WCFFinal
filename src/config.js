4745f30599e99706daefb941d7e301bced78e4ad
Makeja <paul.makeja@ttcl.co.tz>
2026-01-22 12:07:11 +0300

    kamasho main

--- src/config.js
+++ src/config.js
@@ -1,16 +1,7 @@
 // on development
-<<<<<<< HEAD
 // export const baseURL = "http://192.168.21.70:5070/api";
 // export const amiURL = "http://192.168.21.70:5075";
 
 // local
  export const baseURL = "http://127.0.0.1:5070/api";
 export const amiURL = "http://127.0.0.1:5075";
-=======
-export const baseURL = "https://192.168.21.70/api";
-export const amiURL = "http://192.168.21.70:5075";
-
-// local
-// export const baseURL = "http://127.0.0.1:5070/api";
-// export const amiURL = "http://127.0.0.1:5075";
->>>>>>> cc27bedce1b8cb6ce3db5cc5d796ed43d1f9cbec
