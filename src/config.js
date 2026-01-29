d8f546b2fca3c3e9b5a9a325ea6001faa8e6c9cd
Makeja <paul.makeja@ttcl.co.tz>
2026-01-29 14:57:21 +0300

    spy listen update

--- src/config.js
+++ src/config.js
@@ -5,3 +5,5 @@
 // local
  export const baseURL = "http://127.0.0.1:5070/api";
 export const amiURL = "http://127.0.0.1:5075";
+export const ASTERISK_HOST = new URL(baseURL).hostname;
+export const WSS_PORT = 8089;
