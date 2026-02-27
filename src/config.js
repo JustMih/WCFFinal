c830dddea253cf214604e3d84240eb7a9b808868
mikidadimbao48 <mikidadimbao48@gmail.com>
2026-02-27 12:46:04 +0300

    add base url to dns

--- src/config.js
+++ src/config.js
@@ -3,7 +3,8 @@
 // export const amiURL = "http://192.168.21.70:5075";
 
 // local
- export const baseURL = "http://127.0.0.1:5070/api";
+//  export const baseURL = "http://127.0.0.1:5070/api";
+export const baseURL="https://contactcenter.wcf.go.tz/api"
 export const amiURL = "http://127.0.0.1:5075";
 // export const ASTERISK_HOST = new URL(baseURL).hostname;
 // export const WSS_PORT = 8089;
