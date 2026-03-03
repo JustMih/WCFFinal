3dce826df3dde745498ebd20d4bd2bc0262525a8
Ramadhan <aisha.ramadhan@ttcl.co.tz>
2026-03-03 23:00:15 +0300

    minor changes on sip domain ip

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@
 //  export const baseURL = "http://127.0.0.1:5070/api";
 export const baseURL="https://contactcenter.wcf.go.tz/api"
 export const amiURL = "http://127.0.0.1:5075";
-export const SIP_DOMAIN_CONFIG = "https://contactcenter.wcf.go.tz";
+export const SIP_DOMAIN_CONFIG = "contactcenter.wcf.go.tz";
 // export const ASTERISK_HOST = new URL(baseURL).hostname;
 // export const WSS_PORT = 8089;
  
