b46da9ede88354c80bbb89f41ddb778d954a5885
Ramadhan <aisha.ramadhan@ttcl.co.tz>
2026-05-26 10:07:11 +0300

    minor changes

--- src/config.js
+++ src/config.js
@@ -1,16 +1,19 @@
 // on development
-// export const baseURL = "http://192.168.21.70:5070/api";
-// export const amiURL = "http://192.168.21.70:5075";
+export const baseURL = "http://192.168.21.70:5070/api";
+export const amiURL = "http://192.168.21.70:5075";
+export const SIP_DOMAIN_CONFIG = "democc.wcf.go.tz";
 
 // on development
-// export const baseURL = "http://192.168.21.69:5070/api";
-// export const amiURL = "http://192.168.21.69:5075";
+
+// export const baseURL="https://democc.wcf.go.tz"
+// export const amiURL = "http://127.0.0.1:5075";
+// export const SIP_DOMAIN_CONFIG = "democc.wcf.go.tz";
 
 // local
 //  export const baseURL = "http://127.0.0.1:5070/api";
-export const baseURL="https://contactcenter.wcf.go.tz/api"
-export const amiURL = "http://127.0.0.1:5075";
-export const SIP_DOMAIN_CONFIG = "contactcenter.wcf.go.tz";
+// export const baseURL="https://contactcenter.wcf.go.tz/api"
+// export const amiURL = "http://127.0.0.1:5075";
+// export const SIP_DOMAIN_CONFIG = "contactcenter.wcf.go.tz";
 // export const ASTERISK_HOST = new URL(baseURL).hostname;
 // export const WSS_PORT = 8089;
  
