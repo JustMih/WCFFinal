59f48ccd25051c99c4f6dd8acb209979dcb85cdc
reynah762 <reynah762@gmail.com>
2026-05-31 11:34:55 +0300

    config updates

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 // on development
-// export const baseURL = "http://192.168.21.70:5070/api";
-export const amiURL = "http://192.168.21.70:5075";
+
+export const baseURL="https://democc.wcf.go.tz/api"
+export const amiURL = "http://127.0.0.1:5075";
 export const SIP_DOMAIN_CONFIG = "democc.wcf.go.tz";
 export const serverURL = "http://192.168.21.70:5070";
-
-export const baseURL = "http://localhost:5070/api";
